6b84c61f335af4e30d557019c99ce9b29eede983
Anonymous One <jonesaa5@msu.edu>
2021-03-25 14:23:19 -0400

    Auth0 MGMT API Logic WEBAPP-31
    Created the logic for controlling the Auth0 Management API Access Token for requests.

--- backend/blog/utils.py
+++ backend/blog/utils.py
@@ -2,6 +2,35 @@ from django.contrib.auth import authenticate
 import json
 import jwt
 import requests
+from django.conf import settings
+
+from django.core.cache import cache
+import time
+
+from requests.exceptions import RequestException, HTTPError, URLRequired
+
+
+def is_auth0_token_valid(token):
+    """
+    :return: boolean if Auth0 token is valid
+    """
+    exp = jwt.decode(token, verify=False).get('exp')
+    if int(time.time()) >= exp:
+        return False
+    else: return True
+
+
+def refresh_auth0_token():
+    payload = {
+        'grant_type': settings.AUTH0_GRANT_TYPE,
+        'client_id': settings.AUTH0_CLIENT_ID,
+        'client_secret': settings.AUTH0_CLIENT_SECRET,
+        'audience': f'{settings.AUTH0_AUDIENCE}/'
+    }
+    response = requests.post(f'{settings.AUTH0_DOMAIN}/oauth/token', data=payload)
+    oauth = response.json()
+    access_token = oauth.get('access_token')
+    cache.set('Auth0_MGMT_API_JWT', access_token, 84600)
 
 
 def jwt_get_username_from_payload_handler(payload):
