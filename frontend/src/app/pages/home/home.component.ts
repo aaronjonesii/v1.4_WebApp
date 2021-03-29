c783035074d1752ffbbceeffe2d61958962d3606
Anonymous One <jonesaa5@msu.edu>
2021-03-29 11:44:53 -0400

    Prepare Home Component for destroying subscriptions

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,11 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { BlogService } from '../../shared/utils/blog/blog.service';
-import { Subject, Subscription } from 'rxjs';
-import { Blog } from '../../shared/utils/blog/models/blog';
+import { Subject } from 'rxjs';
 import { AuthService } from "@auth0/auth0-angular";
-import { Post } from "../../shared/utils/blog/models/post";
-import { takeUntil } from "rxjs/operators";
-import { StoriesService } from "../../shared/utils/stories.service";
 
 @Component({
   selector: 'app-home',
@@ -14,10 +9,6 @@ import { StoriesService } from "../../shared/utils/stories.service";
 })
 export class HomeComponent implements OnInit, OnDestroy {
   private unsub$: Subject<any> = new Subject<any>();
-  blogs: Blog[] = [];
-  publicStories!: Post[];
-  storiesLoaded = false;
-  sub: Subscription;
   tags = [
     'Technology',
     'Future',
@@ -36,42 +27,14 @@ export class HomeComponent implements OnInit, OnDestroy {
   ];
 
   constructor(
-    private blogService: BlogService,
-    private storiesService: StoriesService,
     public auth: AuthService,
-  ) {
+  ) { }
 
-    this.sub = this.blogService.getBlogSubject().subscribe(blogs => this.blogs = blogs);
-  }
-
-  ngOnInit() {
-    this.blogService.updateBlogSubject();
-    this.getPublicStories();
-  }
+  ngOnInit() { }
 
   ngOnDestroy() {
-    this.sub.unsubscribe();
     this.unsub$.next();
     this.unsub$.complete();
-  }
-
-  getPublicStories() {
-    this.blogService.getPublicPosts().pipe(takeUntil(this.unsub$)).subscribe(
-      stories => {
-        this.publicStories = stories;
-      },
-      error => {
-        // TODO: exclude public page request from HTTPINTERCEPTOR
-        console.error(error);
-        this.publicStories = [];
-        this.storiesLoaded = true;
-      },
-      () => this.complete(),
-    );
-  }
-
-  complete() {
-    this.storiesLoaded = true;
   }
 
 }
