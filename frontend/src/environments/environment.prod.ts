641900af3d539e89d4df0d4c5d3984145000f540
Anonymous One <jonesaa5@msu.edu>
2021-04-03 21:08:50 -0400

    Updated frontend environment variables WEBAPP-69

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  Auth0_domain: 'anonsys.auth0.com',
+  Auth0_clientID: 'J4a9OjrjpSH3A30KuAEJ4AEuM84SD21j',
+  Auth0_audience: 'https://api.anonsys.tech',
+  apiURL: 'http://api.anonsys.tech',
 };
