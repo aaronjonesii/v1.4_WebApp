06a5891838ff6298bb0f4effc46c24013394c5cc
Anonymous One <jonesaa5@msu.edu>
2021-05-10 03:02:31 -0400

    Cleanup format

--- backend/blog/admin.py
+++ backend/blog/admin.py
@@ -5,14 +5,11 @@ from .models import User, Post, Category, Status, Tag, NewsletterSubscriber
 
 class CustomUserAdmin(UserAdmin):
     fieldsets = (
-        (                      # new fieldset added on to the bottom
-            'Frontend Fields',  # group heading of your choice; set to None for a blank space instead of a header
-            {
-                'fields': (
-                    'is_frontend_admin',
-                ),
-            },
+        (                      # new fieldset added on to the top
+            'Frontend Permissions',  # group heading of your choice; set to None for a blank space instead of a header
+            {'fields': ('is_frontend_admin',)},
         ),
+        ('', {'fields': ()}),
         *UserAdmin.fieldsets,  # original form fieldsets, expanded
     )
     list_display = ('username', 'email', 'is_frontend_admin', 'is_staff')
